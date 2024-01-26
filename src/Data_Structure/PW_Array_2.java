1fccbc8de0c7c06e1f6aefb5f9a0cf41304e8dee
Tarun <vennamtarun123@gmail.com>
2024-01-26 18:31:54 +0530

    ujijm

---
+++ src/Data_Structure/PW_Array_2.java
@@ -0,0 +1,50 @@
+package Data_Structure;
+
+import java.util.Arrays;
+import java.util.Scanner;
+public class PW_Array_2 {
+
+    static void PrintArray(int []arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] +" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+//        int arr[] = new int[5];
+        int arr [] = {1,3 , 4 ,5,6};
+        System.out.println("Original Array arr");
+        PrintArray(arr);
+
+        int [] arr_2 = arr;
+        System.out.println("Copied Array of arr");
+//        PrintArray(arr_2);
+//        arr_2[1]= 7;
+//        arr_2[4]=10;
+//        System.out.println("Original arr after changing arr_2 ");
+//        PrintArray(arr);
+//        System.out.println("copied arr after changing arr_2 ");
+//        PrintArray(arr_2);
+//
+//        // From New Feature
+//        int [] arr_3 = arr.clone();
+//        PrintArray(arr_3);
+//        arr_3[1] = 56;
+//        arr_3[4]= 30;
+//        System.out.println("After Change");
+//        PrintArray(arr);
+//        System.out.println("arr_3");
+//        PrintArray(arr_3);
+
+        //CopyOf feature
+        int []arr_4 = Arrays.copyOf(arr,arr.length);
+        PrintArray(arr_4);
+        System.out.println("CopyOfRange");
+        //CopyOfRange
+        int []arr_5 = Arrays.copyOfRange(arr,0,4);
+        PrintArray(arr_5);
+    }
+
+}
